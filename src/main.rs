b3a0f25a60bc2807186c9ef6644f6009445542c6
pullriku <riku3dno.1@gmail.com>
2025-12-14 17:48:09 +0900

    feat: StdIterを実装

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,24 @@ impl MyIterator for Counter {
     }
 }
 
+struct StdIter<T>(T);
+
+impl<T: MyIterator> Iterator for StdIter<T> {
+    type Item = T::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
+    }
+}
+
 fn main() {
     let mut counter = Counter::new();
     println!("{:?}", counter.next()); // Some(0)
     println!("{:?}", counter.next()); // Some(1)
     println!("{:?}", counter.next()); // Some(2)
+
+    // 無限に増加していく
+    for i in StdIter(counter) {
+        println!("{}", i);
+    }
 }
