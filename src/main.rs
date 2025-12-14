1dc1baa174d61da6178bb278424b0046babdf3cb
pullriku <riku3dno.1@gmail.com>
2025-12-14 21:41:55 +0900

    feat: RangeUsize、デモを追加

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,30 @@ impl MyIterator for Counter {
     }
 }
 
+struct RangeUsize {
+    start: usize,
+    end: usize,
+}
+
+impl RangeUsize {
+    fn new(start: usize, end: usize) -> RangeUsize {
+        RangeUsize { start, end }
+    }
+}
+
+impl MyIterator for RangeUsize {
+    type Item = usize;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.start < self.end {
+            let result = self.start;
+            self.start += 1;
+            Some(result)
+        } else {
+            None
+        }
+    }
+}
+
 struct StdIter<T>(T);
 
 impl<T: MyIterator> Iterator for StdIter<T> {
@@ -33,13 +57,29 @@ impl<T: MyIterator> Iterator for StdIter<T> {
 }
 
 fn main() {
-    let mut counter = Counter::new();
-    println!("{:?}", counter.next()); // Some(0)
-    println!("{:?}", counter.next()); // Some(1)
-    println!("{:?}", counter.next()); // Some(2)
+    println!("== MyIteratorを手で回すデモ ==");
 
-    // 無限に増加していく
-    for i in StdIter(counter) {
-        println!("{}", i);
+    let mut range = RangeUsize::new(1, 4);
+    while let Some(x) = range.next() {
+        println!("Raw Range: {}", x);
     }
+
+    println!("\n== StdIterでラップしてforループを使う ==");
+
+    let range_std = StdIter(RangeUsize::new(1, 4));
+    for x in range_std {
+        println!("StdIter: {}", x);
+    }
+
+    println!("\n== 標準メソッドを使いまくる ==");
+
+    let counter_std = StdIter(Counter::new());
+    let vec: Vec<usize> = counter_std
+        .map(|x| x * 10)
+        .filter(|x| x % 4 == 0)
+        .skip(1)
+        .take(10)
+        .collect();
+
+    println!("Result: {vec:?}"); // Result: [20, 40, 60, 80, 100, 120, 140, 160, 180, 200]
 }
